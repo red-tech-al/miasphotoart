d41dbae58fe81c978dfbb2e3511f392b82993c0b
davidguri <david.guri@icloud.com>
2023-11-28 16:09:48 +0100

    (almost) finished nav menu animations

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Layout.module.scss";
 
-import { IoAlbumsOutline, IoCloseOutline } from "react-icons/io5";
+import { IoAlbumsOutline, IoClose } from "react-icons/io5";
 
 export default function Layout({ children }) {
 
@@ -13,42 +13,42 @@ export default function Layout({ children }) {
   }
 
   return (
-    <body className={styles.main}>
-      <section className={styles.content} style={{ transform: `translateY(${(isOpenNav === "false") ? "0%" : "-35%"})` }}>
+    <main className={styles.main}>
+      <button className={styles.navToggle} onClick={() => toggleNav()} style={{ backgroundColor: `rgb(${(isOpenNav === "false") ? "91, 105, 134" : "225, 213, 189"})` }}>
+        {(isOpenNav === "false") ? <IoAlbumsOutline className={styles.icon} style={{ color: "rgb(241, 235, 223)" }} /> : <IoClose className={styles.icon} style={{ color: "rgb(32, 25, 16)" }} />}
+      </button>
+      <section className={styles.content} style={{ transform: `translateY(${(isOpenNav === "false") ? "0%" : "-45%"})` }}>
         {children}
       </section>
       <section className={styles.nav}>
-        <div className={styles.navLinksContainer} style={{ transform: `translateY(${(isOpenNav === "false") ? "120%" : "0%"}) scale(${(isOpenNav === "false") ? "0.9" : "1"})` }}>
+        <div className={styles.navLinksContainer} style={{ transform: `translateY(${(isOpenNav === "false") ? "120%" : "0%"}) scale(${(isOpenNav === "false") ? "0" : "1"})`, height: `${(isOpenNav === "false") ? "0vh" : "45vh"}` }}>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }} className={styles.navLink}>
             <text className={styles.navLinkText}>Home</text>
-            <img className={styles.navImage} src="https://source.unsplash.com/1440x2560" />
+            <div className={styles.navImage} />
           </Link>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }} className={styles.navLink}>
             <text className={styles.navLinkText}>Portfolio</text>
-            <img className={styles.navImage} src="https://source.unsplash.com/1440x2560" />
+            <div className={styles.navImage} />
           </Link>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }} className={styles.navLink}>
             <text className={styles.navLinkText}>Pricing</text>
-            <img className={styles.navImage} src="https://source.unsplash.com/1440x2560" />
+            <div className={styles.navImage} />
           </Link>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }} className={styles.navLink}>
             <text className={styles.navLinkText}>Videography</text>
-            <img className={styles.navImage} src="https://source.unsplash.com/1440x2560" />
+            <div className={styles.navImage} />
           </Link>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }} className={styles.navLink}>
             <text className={styles.navLinkText}>Shop</text>
-            <img className={styles.navImage} src="https://source.unsplash.com/1440x2560" />
+            <div className={styles.navImage} />
           </Link>
           <Link to="/" style={{ color: "inherit", textDecoration: "none" }} className={styles.navLink}>
             <text className={styles.navLinkText}>About</text>
-            <img className={styles.navImage} src="https://source.unsplash.com/1440x2560" />
+            <div className={styles.navImage} />
           </Link>
         </div>
-        <button className={styles.navToggle} onClick={() => toggleNav()}>
-          {(isOpenNav === "false") ? <IoAlbumsOutline className={`${styles.icon} ${styles.openIcon}`} /> : <IoCloseOutline className={`${styles.icon} ${styles.closeIcon}`} />}
-        </button>
       </section>
       <section className={styles.footer}></section>
-    </body>
+    </main>
   );
 }
