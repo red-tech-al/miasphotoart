6c1a38a3b4ae636b601bc3a885702ee85a97ed8f
davidguri <david.guri@icloud.com>
2023-12-13 16:14:12 +0200

    Basically finished the about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -24,7 +24,24 @@ export default function About() {
             </div>
           </section>
           <section id="bio" className={styles.bioSection}>
-            Bio
+            <div className={styles.bioSectionTextContainer}>
+              <text className={styles.bioTitle}>Why are you so drawn to photography?</text>
+              <text className={styles.bioText}>Because I love telling life through the language of images and in doing so I create a connection between fleeting snapshots and eternity - either in color or in black and white.</text>
+            </div>
+            <div className={styles.bioSectionTextContainer}>
+              <text className={styles.bioTitle}>What do you enjoy doing most thematically?</text>
+              <text className={styles.bioText}>Exciting tourism and landscape photography, individual image shoots for small and medium-sized businesses and soulful, emotional wedding reports. I prefer to document authentically and then add that certain something during editing. As a trained tourism manager and coach with international professional experience and a qualified photographer, my focus is the narrative perspective.</text>
+            </div>
+            <div className={styles.bioSectionTextContainer}>
+              <text className={styles.bioTitle}>What makes you special as a photographer?</text>
+              <text className={styles.bioText}>I see myself as an empathetic content designer of a bigger picture.<br />My customers can rely on me and I always do everything I can to achieve the desired effect of individuality. It is important to me to have a good relationship and to guarantee a professional, timely process.<br />I also offer photos in combination with print materials and the production of image videos. Corporate identity orders with selected cooperation partners from the areas of texting and public relations as well as web design and graphics are also possible.
+              </text>
+            </div>
+            <div className={styles.bioSectionTextContainer}>
+              <text className={styles.bioTitle}>What do you do when you're not taking photos?</text>
+              <text className={styles.bioText}>That happens too ;-). Then I'm either researching my next trip or I'm traveling. In winter you can find me on the ski slopes in every free moment and in between I enjoy good food and good conversations with friends.</text>
+              <text className={styles.bioText} style={{ fontWeight: "700", paddingTop: "2.75vh" }}>© Question: Mag. Nora Edelsbacher <a href="http://www.pr-architextur.at" style={{ color: "rgb(91, 105, 134)", textDecoration: "none" }}>( PR Architextur )</a></text>
+            </div>
           </section>
         </main>
       </Fade>
