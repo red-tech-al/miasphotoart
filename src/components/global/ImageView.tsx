f340a1cf2331ae357050bdf2f2d6bc95b17c4def
davidguri <david.guri@icloud.com>
2023-12-22 12:11:05 +0100

    working on smth

--- src/components/global/ImageView.tsx
+++ src/components/global/ImageView.tsx
@@ -2,7 +2,7 @@ import styles from "./styles/ImageView.module.scss"
 
 export default function ImageView(props: any) {
   return (
-    <main className={styles.main} style={{ display: props.show ? "flex" : "none" }} onClick={props.toggleView}>
+    <main className={styles.main} style={{ display: props.show ? "flex" : "flex" }} onClick={props.toggleView}>
       <img src={props.imageUrl} alt={props.imageTitle} className={styles.image} />
     </main>
   );
