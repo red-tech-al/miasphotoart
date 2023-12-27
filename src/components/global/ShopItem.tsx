79e7d45966f693d2d5e25699bfa43fb6eaf72d08
davidguri <david.guri@icloud.com>
2023-12-27 11:40:51 +0100

    fixed depracted function

--- src/components/global/ShopItem.tsx
+++ src/components/global/ShopItem.tsx
@@ -12,7 +12,7 @@ export default function ShopItem() {
     return str.replace(
       /\w\S*/g,
       function (txt: string) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
       }
     );
   }
